38e6e69ecd9e5a82bad4c47aea35e9d7e6ddb3ab
zhushuai <zhushuai@skycomm.com.cn>
2018-05-26 10:20:20 +0800

    koa-static

--- code/bgserver/src/router/index.js
+++ code/bgserver/src/router/index.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react'
-import App from '@containers/App'
-import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Index from '@containers/Index'
+import Login from '@containers/Login'
+import { HashRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom'
 
 export default function() {
   return (
     <Router>
-        <div>
-          <div>
-            <ul>
-              <li><Link to='/index'>Home</Link></li>
-              <li><Link to='/login'>login</Link></li>
-            </ul>
-          </div>
-          <Route path="/" component={App}>
-          </Route>
-        </div>
+      <Switch>
+        <Route path="/index" exact component={Index}/>
+        <Route path="/login" component={Login}/>
+        <Redirect path="/" to={{pathname: '/index'}} />
+      </Switch>
     </Router>
   )
 }
--- code/nodecode/app.js
+++ code/nodecode/app.js
@@ -1,10 +1,16 @@
 const Koa = require('koa');
 const http = require('http');
+const path = require('path');
+const staticResource = require('koa-static');
 const router = require('./router/index.js');
 
 const app = new Koa();
 app.keys = ['zhudaye', 'zhuyeye'];
 
+// 静态资源
+app.use(staticResource(path.join(__dirname, 'static')));
+
+// 挂载路由
 app.use(router.routes())
   .use(router.allowedMethods());
 
