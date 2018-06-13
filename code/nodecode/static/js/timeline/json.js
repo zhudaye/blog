ad06626e74b578a924ea7e0b662e34a5e40abba3
zhushuai <zhushuai@skycomm.com.cn>
2018-06-13 20:33:43 +0800

    时间轴降级

---
+++ code/nodecode/static/js/timeline/json.js
@@ -0,0 +1,57 @@
+var json = {
+	"timeline":
+	{
+		"headline":"The Main Timeline Headline Goes here",
+		"type":"default",
+		"text":"<p>Intro body text goes here, some HTML is ok</p>",
+		"asset": {
+			"media":"http://www.agent-zs.com/img/dcgs.jpg",
+			"credit":"Credit Name Goes Here",
+			"caption":"Caption text goes here"
+		},
+		"date": [
+			{
+				"startDate":"2011,12,10,07,02,10",
+				"endDate":"2011,12,11,08,11",
+				"headline":"Headline Goes Here",
+				"text":"<p>Body text goes here, some HTML is OK</p>",
+				"tag":"This is Optional",
+				"classname":"optionaluniqueclassnamecanbeaddedhere",
+				"asset": {
+					"media":"",
+					"thumbnail":"optional-32x32px.jpg",
+					"credit":"Credit Name Goes Here",
+					"caption":"Caption text goes here"
+				}
+			},
+			{
+				"startDate":"2012,12,10,07,02,10",
+				"endDate":"2012,12,11,08,11",
+				"headline":"Headline Goes Here",
+				"text":"<p>Body text goes here, some HTML is OK</p>",
+				"tag":"This is Optional",
+				"classname":"optionaluniqueclassnamecanbeaddedhere",
+				"asset": {
+					"media":"",
+					"thumbnail":"optional-32x32px.jpg",
+					"credit":"Credit Name Goes Here",
+					"caption":"Caption text goes here"
+				}
+			},
+			{
+				"startDate":"2013,12,10,07,02,10",
+				"endDate":"2013,12,11,08,11",
+				"headline":"Headline Goes Here",
+				"text":"<p>Body text goes here, some HTML is OK</p>",
+				"tag":"This is Optional",
+				"classname":"optionaluniqueclassnamecanbeaddedhere",
+				"asset": {
+					"media":"",
+					"thumbnail":"optional-32x32px.jpg",
+					"credit":"Credit Name Goes Here",
+					"caption":"Caption text goes here"
+				}
+			}
+		]
+	}
+}
